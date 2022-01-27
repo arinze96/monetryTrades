aef1effcd4fa2d303fa9920f71147a06e4838925
Arinzechukwu Edmund <edmund10arinze@gmail.com>
2022-01-27 22:12:17 +0100

    initial commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="{{ asset('assets/images/icons/favicon.ico') }}" />
+    <link rel="icon" type="image/png" href="{{asset('assets/images/background/MT_Trans_Vector.png')}}" />
     <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
     <!--===============================================================================================-->
